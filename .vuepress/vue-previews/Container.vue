292d720a244584a84c7dac27b64c7c6c4ab747ea
chensiyuan <250758092@qq.com>
2023-04-24 09:21:55 +0800

    发

---
+++ .vuepress/vue-previews/Container.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+
+<text-area></text-area>
+<span>调整text-area宽度，这行文字会变颜色</span>
+</div>
+  
+</template>
+ 
+<style lang='scss' scoped>
+div{
+  container: box size;
+
+}
+
+@container box (width > 200px) {
+span {
+  color: red;
+}
+}
+
+</style>
